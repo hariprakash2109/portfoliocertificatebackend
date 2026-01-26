631f677606a8e732523d67a5fca15507a2453fac
hariprakash2109 <hariprakash2109@gmail.com>
2026-01-26 13:40:31 +0530

    updated files

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.MYSQLDATABASE,
   port: process.env.DB_PORT
 });
 
